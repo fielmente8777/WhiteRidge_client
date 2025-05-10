e1cced007d1cb2d21e3af6c1034a9922e1da3713
sumit <sk7627577@gmail.com>
2025-05-10 12:09:53 +0530

    fix(chatbot): update date formatting to use en-GB locale and adjust output format

--- src/components/chatbot/ChatWindow.tsx
+++ src/components/chatbot/ChatWindow.tsx
@@ -100,16 +100,20 @@ const ChatWindow = ({
     setInput(e.target.value);
 
   const handleDateChange = (dateValue: null | Date) => {
-    const value = new Date(dateValue || "").toLocaleDateString();
+    const value = new Date(dateValue || "").toLocaleDateString("en-GB", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
     console.log(value);
-    const [month, day, year] = value.split("/");
+    const [day, month, year] = value.split("/");
 
     if (!value) return;
 
     const currentFlow = messageFlows[currentIndex];
     const key = currentFlow.key || `q${currentIndex}`;
     const answer = `${day}-${month}-${year}`;
-
+    console.log(answer);
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
@@ -609,6 +613,7 @@ const ChatWindow = ({
                       onChange={(date) => handleDateChange(date as Date | null)}
                       showMonthDropdown
                       showIcon
+                      dateFormat={"dd/MM/yyyy"}
                     />
                   </div>
 
